10dd8ac6d356deb3ff86d35dd89586d811d84951
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-09-10 18:01:28 +0100

    created new game class to move responsibilities away from player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,11 +2,24 @@ require 'player'
 
 describe Player do
 
-  let (:gianluca) { Player.new "Gianluca" }
+  subject (:charlotte) { Player.new "Charlotte" }
+  subject (:mittens)  { Player.new "Mittens" }
 
   describe "#name" do
     it "returns the name" do
-      expect(gianluca.name).to eq "Gianluca"
+      expect(charlotte.name).to eq "Charlotte"
+    end
+  end
+
+  describe "#hit_points" do
+    it "returns the hit points" do
+      expect(charlotte.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe "receive_damage" do
+    it "reduces the player hit points" do
+      expect { charlotte.receive_damage}.to change { charlotte.hit_points }.by(-10)
     end
   end
 end
